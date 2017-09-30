c19d9b4aa64f937aeb42d84a690a7f4a0c9bae44
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-09-30 16:03:34 -0300

    agrego el logout

--- suc_android/app/src/main/java/com/example/suc/suc_android_solution/SucStart.java
+++ suc_android/app/src/main/java/com/example/suc/suc_android_solution/SucStart.java
@@ -1,8 +1,18 @@
 package com.example.suc.suc_android_solution;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SucStart extends AppCompatActivity {
 
@@ -12,5 +22,42 @@ public class SucStart extends AppCompatActivity {
         setContentView(R.layout.activity_suc_start);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.suc_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_logout){
+            AccountManager accountManager = AccountManager.get(getBaseContext());
+            Account[] accounts = accountManager.getAccountsByType(AuthConfig.KEY_ACCOUNT_TYPE.getConfig());
+            AccountManagerCallback<Boolean> callback = new AccountManagerCallback<Boolean>() {
+                @Override
+                public void run(AccountManagerFuture<Boolean> future) {
+                    if(future.isDone()){
+                        try{
+                            if(future.getResult()){
+                                Intent loginIntent = new Intent(getBaseContext(), SucMain.class);
+                                startActivity(loginIntent);
+                            }else{
+                                Toast.makeText(getBaseContext(), "Hubo un error cerrando la sesion", Toast.LENGTH_SHORT).show();
+                            }
+                        }catch (Exception ex){
+                            Toast.makeText(getBaseContext(), "Hubo un error cerrando la sesion", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            };
+
+            accountManager.removeAccount(accounts[0],callback,null);
+        }
+
+        return true;
     }
 }
