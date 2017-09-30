d87aaac1db244938f88cb93dfec533a34ffa75b9
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-09-30 13:37:55 -0300

    mejoramos el codigo, y mostramos mejor el registro de cuentas en al accountMnager de android

---
+++ suc_android/app/src/main/java/com/example/suc/suc_android_solution/SucStart.java
@@ -0,0 +1,16 @@
+package com.example.suc.suc_android_solution;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class SucStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suc_start);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
