3506b447fa0530b842e702df3b33e8aa0801ea25
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-11-24 08:38:56 -0300

    algunas mejoras en el mapa

--- suc_android/app/src/main/java/com/example/suc/suc_android_solution/NearestDinersFragment.java
+++ suc_android/app/src/main/java/com/example/suc/suc_android_solution/NearestDinersFragment.java
@@ -365,7 +365,6 @@ public class NearestDinersFragment extends Fragment implements
         }
 
         mGoogleMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
-
             @Override
             public void onCameraMove() {
                 float[] distanceArray = new float[3];
@@ -378,16 +377,16 @@ public class NearestDinersFragment extends Fragment implements
                         location.getLatitude(),
                         location.getLongitude(), distanceArray);
                 if (distanceArray[0] > 2000) {
+                    location.setLatitude(actualLatLng.latitude);
+                    location.setLongitude(actualLatLng.longitude);
                     final Snackbar snackbar = Snackbar
-                            .make(mView, "Buscar comedores en esta zona?", BaseTransientBottomBar.LENGTH_INDEFINITE)
+                            .make(mView, "Buscar comedores en esta zona?", BaseTransientBottomBar.LENGTH_SHORT)
                             .setAction("Dale", new View.OnClickListener() {
 
                                 @Override
                                 public void onClick(View view) {
                                     if (getterTask == null) {
                                         initializeGetterTask();
-                                        location.setLatitude(actualLatLng.latitude);
-                                        location.setLongitude(actualLatLng.longitude);
                                         getterTask.execute(String.valueOf(actualLatLng.latitude), String.valueOf(actualLatLng.longitude));
                                     }
                                 }
@@ -398,7 +397,6 @@ public class NearestDinersFragment extends Fragment implements
                 }
             }
         });
-
 
         String latitude = ARGENTINA.get("latitude");
         String longitude = ARGENTINA.get("longitude");
@@ -481,6 +479,7 @@ public class NearestDinersFragment extends Fragment implements
             @Override
             public void onMarkersReady(ArrayList<MapMarkerViewModel> markers) {
                 if (markers.size() > 0) {
+                    selectedItem = 0;
                     MapMarkerViewModel centerMarker = markers.get(0);
                     centerItemPinId = centerMarker.getPinId();
                     setMarkers(centerMarker, markers);
@@ -702,7 +701,7 @@ public class NearestDinersFragment extends Fragment implements
 
         private Bitmap createSmallBitmapPin(@DrawableRes int id) {
             Bitmap bmp = BitmapFactory.decodeResource(context.getResources(), id);
-            Bitmap result = Bitmap.createScaledBitmap(bmp, (int) (150 * PIN_SCALE), (int) (150 * PIN_SCALE), false);
+            Bitmap result = Bitmap.createScaledBitmap(bmp, (int) (150 ), (int) (150), false);
 
             Paint markerPaint = new Paint();
             markerPaint.setAntiAlias(true);
@@ -837,7 +836,8 @@ public class NearestDinersFragment extends Fragment implements
         viewPager.setCurrentItem(initialItem);
         viewPager.init(mMapView);
         // Selects initial position
-        mapNavigator.setMarkerVisibleAndSelected(initialItem, true);
+
+        //mapNavigator.setMarkerVisibleAndSelected(initialItem, true);
     }
 
 
