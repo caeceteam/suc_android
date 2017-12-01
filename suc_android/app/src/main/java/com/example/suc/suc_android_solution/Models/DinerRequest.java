2b31f5f4c40ea636e2b03f62e40cf8d1e7f90547
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-12-01 19:38:15 -0300

    hacemos que la lista de dinerRequest no tenga valores duplicados

--- suc_android/app/src/main/java/com/example/suc/suc_android_solution/Models/DinerRequest.java
+++ suc_android/app/src/main/java/com/example/suc/suc_android_solution/Models/DinerRequest.java
@@ -14,4 +14,26 @@ public class DinerRequest {
     public String title;
     public String description;
     public Date creationDate;
+
+    @Override
+    public boolean equals(Object obj) {
+        DinerRequest other = (DinerRequest) obj;
+        return other.idDinerRequest.equals(this.idDinerRequest) &&
+                other.idDiner.equals(this.idDiner) &&
+                other.title.equals(this.title) &&
+                other.description.equals(this.description) &&
+                other.creationDate.equals(this.creationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result * this.idDinerRequest.hashCode();
+        result = 31 * result * this.idDiner.hashCode();
+        result = 31 * result * this.title.hashCode();
+        result = 31 * result * this.description.hashCode();
+        result = 31 * result * this.creationDate.hashCode();
+
+        return result;
+    }
 }
